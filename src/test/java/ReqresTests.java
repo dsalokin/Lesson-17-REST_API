0a7615178646fd95888e1c47bf5087c6ba6d66e8
Hermit91 <work.salokin@gmail.com>
2021-12-29 02:22:05 +0300

    added ReqresTestsTests

---
+++ src/test/java/ReqresTests.java
@@ -0,0 +1,52 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresTests {
+
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "https://reqres.in/";
+    }
+
+    @Test
+    void successfulLogin() {
+        // https://reqres.in/api/login
+        /*
+            {
+                "email": "eve.holt@reqres.in",
+                "password": "cityslicka"
+            }
+         */
+        //  "token": "QpwL5tke4Pnpja7X4"
+        //  200
+
+        String data = "{ \"email\": \"eve.holt@reqres.in\", \"password\": \"cityslicka\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("/api/login")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+    @Test
+    void negativeLogin() {
+        String data = "{ \"email\": \"peter@klaven\" }";
+
+        given()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("/api/login")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+}
